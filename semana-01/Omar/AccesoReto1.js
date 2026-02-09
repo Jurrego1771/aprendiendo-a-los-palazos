ab05452d99cd98b57c96f552b7817deeb68e935f
omnio52 <mapoma@gmail.com>
2026-02-08 21:28:09 -0500

    Reto1 Acceso con return

--- semana-01/Omar/AccesoReto1.js
+++ semana-01/Omar/AccesoReto1.js
@@ -4,31 +4,17 @@
 // Función validar de acceso
 
 function validarAcceso(nombre, edad) {
-  
-  console.log("Nombre recibido:", nombre);
-  console.log("Edad recibida:", edad);
-  
-// se agrega validar que nombre sea tipo string y maximo 20 caracteres, Edad tipo Int. si no se cumple aparece mensaje por consola
-
-  const nombreValido = typeof nombre === 'string' && /^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(nombre);
-  console.log("¿Nombre válido?", nombreValido);
-  
-  const longitudValida = nombre.length <= 20;
-  console.log("¿Longitud válida?", longitudValida);
-  
-  const edadValida = Number.isInteger(edad) && edad >= 0;
-  console.log("¿Edad válida?", edadValida);
-  
-  // Operador ternario
-
-  !nombreValido ? console.log("Error: El nombre solo debe contener caracteres") :
-  !longitudValida ? console.log("Error: El nombre no puede exceder 20 caracteres") :
-  !edadValida ? console.log("Error: La edad solo debe contener números naturales") :
-  edad >= 18 ? console.log(`Bienvenido ${nombre}`) :
-  console.log("Acceso denegado");
-  
-  
+  //se agrega validar que nombre sea tipo string y maximo 20 caracteres, Edad tipo Int. si no se cumple aparece mensaje por consola
+  // validación con un return
+  typeof nombre !== 'string' || !/^[a-zA-ZáéíóúÁÉÍÓÚñÑ\s]+$/.test(nombre) ? 
+    (console.log("Error: El nombre solo debe contener caracteres"), undefined) : 
+  nombre.length > 20 ? 
+    (console.log("Error: El nombre no puede exceder 20 caracteres"), undefined) : 
+  !Number.isInteger(edad) || edad < 0 ? 
+    (console.log("Error: La edad solo debe contener números"), undefined) : 
+  edad >= 18 ? 
+    console.log(`Bienvenido ${nombre}, tienes ${edad} años`) : 
+    console.log("Acceso denegado");
 }
-
 // Prueba
-validarAcceso("Ana1234", 25);
+validarAcceso("Ana maria", "25");
